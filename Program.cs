1cd17a962b1dcbbcc2a2d147965c7464a44393e2
mickelsonm <mickelmr@gmail.com>
2019-01-28 16:11:36 -0600

    init checkin

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace csharp_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ScrambleOne("tester"));
+            Console.WriteLine(ScrambleTwo("tester"));
+            Console.WriteLine(ScrambleThree("tester"));
+        }
+
+        private static string ScrambleOne(string input){
+          string result = "";
+          int rndpos;
+          Random rnd = new Random();
+
+          //create character set using copy of provided input string
+          string charset = String.Copy(input);
+
+          while(charset.Length > 0){
+            //pick a random position given provided characters
+            rndpos = rnd.Next(0, charset.Length);
+            //append selected position's character to our result
+            result += charset[rndpos];
+            //remove selected position so it's not repeated
+            charset = charset.Remove(rndpos, 1);
+          }
+
+          return result;
+        }
+
+        private static string ScrambleTwo(string input){
+          int rndpos;
+          Random rnd = new Random();
+          char current;
+          char[] chars = input.ToCharArray();
+
+          for(int i=0; i < chars.Length; i++){
+            //pick a random position from available characters
+            rndpos = rnd.Next(0, chars.Length);
+            //store current character
+            current = chars[i];
+            //swap current and randomly picked character
+            chars[i] = chars[rndpos];
+            chars[rndpos] = current;
+          }
+          
+          return new string(chars);
+        }
+
+        private static string ScrambleThree(string input){
+          string result = "";
+          return result;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Challenge Text
+
+Write 3 functions (each with a different implementation) which accept a
+string and return a string containing the same characters as the input
+string -- but in a random order. Compare the 3 implementations and
+describe why you might prefer one implementation over another
+(and under what circumstances). Two of the implementations should use
+the System.Random type and its Next(int minValue, int maxValue)
+overload, while the third implementation isn't constrained to
+System.Random usage.
+
+# Methods Descriptions
+
+```csharp
+private static string ScrambleOne(string input)
+```
+
+This method makes a copy of the input string to ensure the original remains
+unchanged.
+
+Next, it loops over all of the characters, picks a random character, adds that
+character to the result string, and removes it from the available characters to
+prevent it from being used again.
+
+Once work has been completed it returns the result string.
+
+```csharp
+private static string ScrambleTwo(string input)
+```
+
+This method converts the input string to a character array.
+
+Next, it loops over the characters in array and while doing so it picks a random character
+to be swapped out with. For example, if given an input string of 'ABC', on the first
+iteration 'A' is the current character and if 'C' is randomly selected then these are
+swapped. So, 'A' would become 'C' and 'C' would become 'A'.
+
+Once these swaps have been made within the array, then we simply construct a new
+string that serves as our result.
